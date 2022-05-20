13c5b11afc405f3ecf0f3926b3311c597ed07361
Alick <AlickGZ9@gmail.com>
2022-05-20 04:33:19 +0300

    added resetPosition and onSelect handler

--- src/manipulation/move.ts
+++ src/manipulation/move.ts
@@ -15,36 +15,39 @@ export const installMoveObjects = (space: iSpace, options?: iCanvasMoveOptions) 
         pos: Point(),
         object: undefined,
     }
-    return setupMoveEvents(space, {
-        startMove(pos: iPosition){
-            const object = findUnderTouch({space, pos, filter: (o) => o.movable && o.selected()});
-            if (object){
-                Object.assign(config, {
-                    pos: Point(object.position.center),
-                    object
-                });
-                return true;
-            };
-            return false;
-        },
-        move(pos: iPosition, shift: iPosition){
-            if (config.object)
-                config.object.position.center = config.pos.add(shift);
-        },
-        endMove(){
-            config.object = undefined;
-        },
-        scale(factor: number){
-            if (config.object){
-                const pos = config.object.position;
-                pos.scale = (pos.scale || 1) * factor;
+    return {
+        ...setupMoveEvents(space, {
+            startMove(pos: iPosition){
+                const object = findUnderTouch({space, pos, filter: (o) => o.movable && o.selected()});
+                if (object){
+                    Object.assign(config, {
+                        pos: Point(object.position.center),
+                        object
+                    });
+                    return true;
+                };
+                return false;
+            },
+            move(pos: iPosition, shift: iPosition){
+                if (config.object)
+                    config.object.position.center = config.pos.add(shift);
+            },
+            endMove(){
+                config.object = undefined;
+            },
+            scale(factor: number){
+                if (config.object){
+                    const pos = config.object.position;
+                    pos.scale = (pos.scale || 1) * factor;
+                }
+            },
+            rotate(dAngle: number, rotationCenter: iPosition){
+                if (config.object){
+                    changeRotationCenter(config.object.transform(rotationCenter), config.object.position);
+                    config.object.position.angle = (config.object.position.angle || 0) + dAngle;
+                }
             }
-        },
-        rotate(dAngle: number, rotationCenter: iPosition){
-            if (config.object){
-                changeRotationCenter(config.object.transform(rotationCenter), config.object.position);
-                config.object.position.angle = (config.object.position.angle || 0) + dAngle;
-            }
-        }
-    }, options);
+        }, options),
+
+    }
 };
