b4b39ba527441393304294eecd62e7f47e7fde9c
Alick <AlickGZ9@gmail.com>
2022-07-25 01:30:51 +0300

    export setupMoveEvents in global

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export {Space} from './space';
 export {installSelection, installClicks} from './manipulation/select';
 export {installCanvasControll} from './manipulation/canvas';
 export {installMoveObjects} from './manipulation/move';
+export {setupMoveEvents} from './manipulation/movementEvents';
 export {SpriteDrawer} from './sprite';
