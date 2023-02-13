7c10db825a68f9ffeeaa85030ea9f5c3614b7720
Alick <AlickGZ9@gmail.com>
2023-02-13 18:51:31 +0100

    add original event to pos event listener

--- src/manipulation/events.ts
+++ src/manipulation/events.ts
@@ -1,11 +1,11 @@
 import { iPosition } from "../positioning"
 import { iSpace } from "../space";
 
-type tPosListener = (pos: iPosition) => void;
+type tPosListener = (pos: iPosition, e: MouseEvent) => void;
 type tPosEvent = 'click' | 'mousedown' | 'mouseup' | 'mousemove' | 'contextmenu';
 
 export const addPosEventListener = (space: iSpace, eventName: tPosEvent, listener: tPosListener) => {
     space.canvas.addEventListener(eventName, (e: MouseEvent) => {
-        listener({x: e.offsetX, y: e.offsetY});
+        listener({x: e.offsetX, y: e.offsetY}, e);
     })
 }
